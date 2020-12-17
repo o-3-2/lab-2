e775a7b2a7376f4ba2a91851475e3721ac883545
o-3-2 <74115131+o-3-2@users.noreply.github.com>
2020-12-17 12:32:34 +0400

    Update Stream.java

--- lab 2 OC/src/Stream.java
+++ lab 2 OC/src/Stream.java
@@ -1,39 +1,45 @@
 public class Stream {
 
-	private int maxTimeOfStream;
-	private String streamId;
+    private int maxTimeOfStream;
+    private String streamId;
+    private int prioritet;
 
-	private String procId;
+    public Stream(String streamId, int maxTimeOfStream, int prioritet) {
+        this.streamId = streamId;
+        this.maxTimeOfStream = maxTimeOfStream;
+        this.prioritet = prioritet;
+    }
 
-	public Stream(String streamId, int maxTimeOfStream, String procId) {
-		this.streamId = streamId;
-		this.procId = procId;
-		this.maxTimeOfStream = maxTimeOfStream;
+    public String getStreamId() {
+        return streamId;
+    }
 
-	}
+    public int getMaxTimeOfStream() {
+        return maxTimeOfStream;
+    }
+
+    public boolean isTime() {
+        if (maxTimeOfStream <= 0) {
+            return false;
+        }
+        return true;
+    }
+
+    public void timeReduction() {
+        maxTimeOfStream--;
+    }
+
+    public int getPrioritet() {
+        return prioritet;
+    }
+
+    public void Change() {
+        prioritet = 0;
+    }
 
 
-	public String getStreamId() {
-		return streamId;
-	}
-
-	public String getProcId() {
-		return procId;
-	}
-
-	public int getMaxTimeOfStream() {
-		return maxTimeOfStream;
-	}
-
-	public boolean isTime() {
-		if (maxTimeOfStream <= 0) {
-			return false;
-		}
-		return true;
-	}
-
-	public void timeReduction() {
-		maxTimeOfStream--;
-	}
-
+    public void setPrioritet(int prioritet) {
+        this.prioritet = prioritet;
+    }
 }
+
