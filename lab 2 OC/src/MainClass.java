201d07202d5062173d62ce898c50b31eb73ab3d6
RAFMAN <volkov.rafman@mail.ru>
2020-12-21 14:30:56 +0400

    Revert "Update file"
    This reverts commit 42bcbb72

--- lab 2 OC/src/MainClass.java
+++ lab 2 OC/src/MainClass.java
@@ -1,7 +1,8 @@
 
 public class MainClass {
-    public static void main(String[] args) {
-        Plan plan = new Plan();
-        plan.Start();
-    }
+	public static void main(String[] args) {
+		Plan plan = new Plan();
+		 plan.Start();
+	}
+
 }
--- lab 2 OC/src/Plan.java
+++ lab 2 OC/src/Plan.java
@@ -2,56 +2,42 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class Plan {
-    public static final int TIME = 5;
-    ArrayList<Process> Processes;
-    ArrayList<Stream> Streams;
-    private int maxPrioritet = -1;
-    private int k;
+	public static final int TIME = 5;
+	ArrayList<Process> Processes;
+	private int maxPrioritet = -1;
+	private int k;
+	public Plan() {
+		Processes = new ArrayList<Process>();
+		Random length = new Random();
+		for (int i = 0; i < length.nextInt(10) + 1; i++) {
+			int prioritet = length.nextInt(100) + 1;
+			if (prioritet < maxPrioritet || maxPrioritet == -1) {
+				maxPrioritet = prioritet;
+				k = i;
+			}
+			Process proc = new Process("ïðîöåññ:" + (i + 1), prioritet);
+			Processes.add(proc);
+		}
+	}
+	
+	public void Start() {
+		int i = maxPrioritet;
+		int j = 0;
+		while(j < Processes.size())
+		{
+			Processes.get(k).start(TIME);
+			Processes.get(k).Change();
+			i = 0;
+			for(int l = 0; l < Processes.size(); l++) {	
+				if((Processes.get(l).getprioritet() < i || i == 0) && Processes.get(l).getprioritet() != 0) {
+					i = Processes.get(l).getprioritet();
+					k = l;
+				}
+			}
+			j++;
+		}
+		
+	}
 
-    public Plan() {
-        Processes = new ArrayList<Process>();
-        Random length = new Random();
 
-        Streams = new ArrayList<Stream>();
-        for (int i = 0; i < length.nextInt(7) + 1; i++) {
-            int prioritet = length.nextInt(100) + 1;
-            if (prioritet < maxPrioritet || maxPrioritet == -1) {
-                maxPrioritet = prioritet;
-                k = i;
-            }
-            Streams.add(new Stream("Ïîòîê " + (i + 1), 8 * i + 1, prioritet));
-        }
-    }
-
-    public void Start() {
-        while (!Streams.isEmpty()) {
-            for (int i = 0; i < Streams.size(); i++) {
-                int flag = TIME;
-                while (Streams.get(i).isTime() && flag > 0) {
-                    print(" ïðîöåññ", i);
-                    Streams.get(i).timeReduction();
-                    flag--;
-                    if (flag == 0 && Streams.get(i).getMaxTimeOfStream() != 0) {
-                        print(" ïðèîñòîíîâëåí ", i);
-                        System.out.println();
-                        break;
-                    }
-                    if (Streams.get(i).getMaxTimeOfStream() == 0) {
-                        print(" çàâåðøåí ", i);
-                        System.out.println();
-                        Streams.remove(i);
-                        --i;
-                        break;
-                    }
-                }
-            }
-        }
-    }
-
-    public void print(String str, int i) {
-        System.out.println(Streams.get(i).getStreamId() + " ïðèîðèòåò : "
-                + Streams.get(i).getPrioritet() + " âðåìÿ: "
-                + Streams.get(i).getMaxTimeOfStream()
-                + str);
-    }
 }
